7051cfc9f0171db4418ae090cdf897e435312d31
Kens-front <knstntnpasecnik@gmail.com>
2026-04-08 18:38:03 +0300

    поменял время модалки

--- landing2/app/app.vue
+++ landing2/app/app.vue
@@ -162,7 +162,7 @@ onMounted(() => {
 	const container = horizontalContainer.value
 
 	gsap.to(aside.value, {x: 0, delay: 4})
-  gsap.to(isVisibleModal, {value: true, delay: 9});
+  gsap.to(isVisibleModal, {value: true, delay: window.innerWidth < 1024 ? 9 : 6});
 	gsap.fromTo('body', {overflow: 'hidden'}, {overflow: 'visible', delay: 4})
 	// Горизонтальная анимация для первых трех секций
 	gsap.to(sections, {
